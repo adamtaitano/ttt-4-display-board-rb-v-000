18d35cb4b8427a6f048368e111be01a86abc0862
Adam Taitano <adam.taitano@gmail.com>
2018-06-21 22:59:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(array = ["","","","","","","","",""])
+  board = ""
+
+  line = "-----------"
+  
+  row_one = " #{array[0] | "
+  row_two =
+  row_three = 
+end
